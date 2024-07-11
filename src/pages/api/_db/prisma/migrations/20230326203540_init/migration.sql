d71f2682004d2b60a6962abb43a9c7f460d4a23d
Diogo Sant'Anna <diogosantanna08@gmail.com>
2024-07-11 13:28:06 -0300

    ♻️ refactor: substitui banco de dados mysql po sqlite in-memory

--- src/pages/api/_db/prisma/migrations/20230326203540_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE `UsersItems` DROP FOREIGN KEY `UsersItems_itemId_fkey`;
-
--- DropForeignKey
-ALTER TABLE `UsersItems` DROP FOREIGN KEY `UsersItems_userId_fkey`;
-
--- RenameIndex
-ALTER TABLE `UsersItems` RENAME INDEX `UsersItems_userId_fkey` TO `UsersItems_userId_idx`;
